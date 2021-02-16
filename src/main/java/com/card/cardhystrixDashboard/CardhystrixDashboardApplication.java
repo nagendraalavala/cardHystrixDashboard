6508a0e9c3539bb64d0f6202338b41114c2913bc
Phanialavala3 <54546788+Phanialavala3@users.noreply.github.com>
2021-02-16 02:26:28 -0600

    initial

---
+++ src/main/java/com/card/cardhystrixDashboard/CardhystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.card.cardhystrixDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardhystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardhystrixDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/card/cardhystrixDashboard/CardhystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.card.cardhystrixDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardhystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
