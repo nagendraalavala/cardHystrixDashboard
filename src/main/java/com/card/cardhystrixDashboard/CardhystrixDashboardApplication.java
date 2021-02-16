c2dee2033bb5c7c0eb5390ae0a86d63752f661ad
Phanialavala3 <54546788+Phanialavala3@users.noreply.github.com>
2021-02-16 02:46:53 -0600

    kk

--- src/main/java/com/card/cardhystrixDashboard/CardhystrixDashboardApplication.java
+++ src/main/java/com/card/cardhystrixDashboard/CardhystrixDashboardApplication.java
@@ -2,8 +2,12 @@ package com.card.cardhystrixDashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableHystrixDashboard
 public class CardhystrixDashboardApplication {
 
 	public static void main(String[] args) {
